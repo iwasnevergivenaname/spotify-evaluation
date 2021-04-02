5f020d7b29388986589ff7387516bc61861bcf25
iwasnevergivenaname <hannah.mace93@gmail.com>
2021-04-02 12:25:36 -0700

    functional front end

---
+++ README.md
@@ -0,0 +1,93 @@
+#spotify evaluation 
+welcome to this Flask personality evaluation app! judgements are made using a users available Spotify data 
+
+##getting started
+before you run this code on your own machine, please log in and register an app with the
+[Spotify for Developers API](https://developer.spotify.com/dashboard/).
+you will need the client id and client secret that will be provided for each app you create on there. and then register 
+http://127.0.0.1:5000/callback as the Redirect URI (found when you click the Edit Settings button).
+
+in your terminal run 
+```shell script
+source venv/bin/activate
+pip3 install requirements.txt
+flask run
+``` 
+and open a web browser to [127.0.0.1:5000](http://127.0.0.1:5000/)
+
+## about the application
+this app is just a personal take on Personality Quizzes using the ideology of Moral Alignment from Dungeons and Dragons
+
+#### how it works
+this app utilizes Spotify's API and OAuth. after a user connect their spotify account to the app, the app makes calls to
+Spotify requesting information about the Users display information, top Tracks, Artists, and Playlist. using the combined
+data of those Tracks and Artists, an light hearted judgement is made and returned to the user. this app is meant to be
+fun, while exploring data visualization. 
+
+#### judgement criteria 
+everything is based off of the idea that music falling in specific ranges of data fall into one of these six categories,
+Lawful Good, Lawful Neutral, Lawful Evil, Neutral Good, True Neutral, Neutral Evil, Chaotic Good, Chaotic Neutral, Chaotic Evil.
+
+the data in question would be the "audio features" returned for specific Tracks by Spotify. the information returned was
+then pared down just to fields that were all measured by equal units. this left Popularity, Energy, Danceability, 
+Acousticness, Speechiness, and Valence.
+
+|      | pop | energy | dance | acoustic | speech | valence |
+|------|-----|--------|-------|----------|--------|---------|
+| mean | 26% | 48% | 54% | 50% | 11% | 52% |
+| std | 22% | 27% | 18% | 38% | 18% | 26% |
+| 50% | 25% | 47% | 55% | 52% | 5% | 54% |
+| 75% | 42% | 71% | 70% | 90% | 8% | 74% |
+
+low range will be considered anything below 30%, mid range is 30%-60%, and high range is 60%+ 
+(speechiness will be treated slightly differently since it's an outlier)
+
+my arbitrary alignment is as follows:
+
+|      | Lawful | Neutral | Chaotic |
+|------|-----|--------|-------
+| Good | high dance | high dance  | high dance | 
+|  | high valence | mid range everything else  | high energy| 
+|  | |  | low pop | 
+| Neutral | high acoustic | mid range everything | high energy | 
+|  | low everything else | | mid valence | 
+|  | |  | low pop | 
+| Evil | low dance | low everything | high energy |
+| | high acoustic |  | low dance |
+| | high speech |  | low pop |
+
+
+
+## some code to know
+
+#### techstack
+- Flask
+- Spotify's Developers API
+
+
+
+## visuals
+
+![evolution of the homepage design](https://i.imgur.com/ZbgKAV7.png)
+here is the evolution of the homepage webdesign 
+
+using Spotify's green for initial inspiration, i was able to eventually find a match between Spotify and my own taste.
+
+
+## routes
+
+| Route Name | Description |
+|------------|-------------|
+| / | homepage |
+| /connect | spotify redirect |
+| /callback | registered redirect uri |
+| /about | about page |
+
+
+## future updates
+
+## thank you
+
+## references 
+[Spotify Flask Auth Example by drshrey](spotify-flask-auth-example)
+
---
+++ app.py
@@ -0,0 +1,118 @@
+import json
+from flask import Flask, request, redirect, g, render_template, session
+import requests
+from urllib.parse import quote
+import os
+
+app = Flask(__name__)
+
+client_id = os.environ.get("CLIENT_ID")
+client_secret = os.environ.get("CLIENT_SECRET")
+spotify_auth_url = "https://accounts.spotify.com/authorize"
+spotify_token_url = "https://accounts.spotify.com/api/token"
+spotify_api_base = "https://api.spotify.com"
+API_VERSION = "v1"
+spotify_api_url = f"{spotify_api_base}/{API_VERSION}"
+
+CLIENT_SIDE_URL = "http://127.0.0.1"
+PORT = 5000
+redirect_uri = os.environ.get("REDIRECT_URI")
+# specify what permissions you need based on endpoints
+scope = "user-read-private user-read-email user-top-read playlist-modify-public playlist-modify-private"
+state = ""
+SHOW_DIALOG_bool = True
+SHOW_DIALOG_str = str(SHOW_DIALOG_bool).lower()
+
+app.config["SECRET_KEY"] = "secrets secrets are no fun"
+
+# the following spotify authentication code was taken and adapted slightly from this repo
+# https://github.com/drshrey/spotify-flask-auth-example/blob/master/main.py
+
+auth_query_parameters = {
+	"response_type": "code",
+	"redirect_uri": redirect_uri,
+	"scope": scope,
+	"client_id": client_id
+}
+
+
+@app.route("/")
+def homepage():
+	return render_template('connect.html')
+
+
+@app.route("/connect")
+def spotify_auth():
+	# app requests authorization ie redirect to spotify login page
+	url_args = "&".join([f'{key}={quote(val)}' for key, val in auth_query_parameters.items()])
+	auth_url = f"{spotify_auth_url}/?{url_args}"
+	return redirect(auth_url)
+
+
+@app.route("/callback")
+def callback():
+	# requests refresh and access tokens
+	auth_token = request.args['code']
+	session['code'] = auth_token
+	
+	auth_options = {
+		"grant_type": "authorization_code",
+		"code": str(auth_token),
+		"redirect_uri": redirect_uri,
+		'client_id': client_id,
+		'client_secret': client_secret,
+	}
+	post_req = requests.post(spotify_token_url, data=auth_options)
+	
+	# token returned to app
+	resp_data = json.loads(post_req.text)
+	access_token = resp_data["access_token"]
+	session['access_token'] = access_token
+	
+	refresh_token = resp_data["refresh_token"]
+	token_type = resp_data["token_type"]
+	expires_in = resp_data["expires_in"]
+	
+	# access token to access api
+	auth_header = {"Authorization": f"Bearer {access_token}"}
+	
+	# profile data
+	user_profile_endpoint = f"{spotify_api_url}/me"
+	profile_resp = requests.get(user_profile_endpoint, headers=auth_header)
+	profile_data = json.loads(profile_resp.text)
+	
+	# user top artists
+	user_top_artist_endpoint = f"{spotify_api_url}/me/top/artists?limit=5"
+	top_artist_resp = requests.get(user_top_artist_endpoint, headers=auth_header)
+	top_artist_data = json.loads(top_artist_resp.text)
+	
+	# user top tracks
+	user_top_tracks_endpoint = f"{spotify_api_url}/me/top/tracks?limit=25"
+	top_tracks_resp = requests.get(user_top_tracks_endpoint, headers=auth_header)
+	top_tracks_data = json.loads(top_tracks_resp.text)
+	
+	# user playlist data
+	playlist_endpoint = f"{profile_data['href']}/playlists"
+	playlists_resp = requests.get(playlist_endpoint, headers=auth_header)
+	playlist_data = json.loads(playlists_resp.text)
+	
+	return render_template("user_info.html", profile=profile_data, artists=top_artist_data, tracks=top_tracks_data)
+
+
+@app.route("/<track_id>")
+def show_track_data(track_id):
+	# access token to access api
+	access_token = session['access_token']
+	auth_header = {"Authorization": f"Bearer {access_token}"}
+	
+	# track features like loudness and tempo
+	track_features_endpoint = f"{spotify_api_url}/audio-features/{track_id}"
+	track_features_resp = requests.get(track_features_endpoint, headers=auth_header)
+	track_features_data = json.loads(track_features_resp.text)
+	
+	return render_template('track_analysis.html', info=track_features_data)
+
+@app.route("/about")
+def about_page():
+	# about page
+	return render_template("about.html")
