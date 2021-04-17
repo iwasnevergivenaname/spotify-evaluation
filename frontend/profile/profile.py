aca6882d80457053f5bdb772ebcf5611337d3098
iwasnevergivenaname <hannah.mace93@gmail.com>
2021-04-17 13:56:56 -0700

    working auth and connected to DB using flask blueprint

---
+++ frontend/profile/profile.py
@@ -0,0 +1,57 @@
+from flask import render_template, Blueprint, session, redirect
+from flask import current_app as app
+import requests
+import json
+from flask_sqlalchemy import SQLAlchemy
+from models import User, connect_db, db
+
+# Blueprint Configuration
+profile_bp = Blueprint(
+	'profile_bp', __name__,
+	template_folder='templates',
+	static_folder='static'
+)
+
+# get the models up and running
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///spotify_evaluation'
+connect_db(app)
+db.create_all()
+
+#  spotify
+spotify_api_base = "https://api.spotify.com"
+API_VERSION = "v1"
+spotify_api_url = f"{spotify_api_base}/{API_VERSION}"
+
+
+@profile_bp.route('/profile', methods=['GET'])
+def profile():
+	# access token to access api
+	if not session.get('access_token'):
+		return redirect("/connect")
+	else:
+		access_token = session['access_token']
+		auth_header = {"Authorization": f"Bearer {access_token}"}
+
+		# profile data
+		user_profile_endpoint = f"{spotify_api_url}/me"
+		profile_resp = requests.get(user_profile_endpoint, headers=auth_header)
+		profile_data = json.loads(profile_resp.text)
+
+		spotify_id = profile_data['id']
+
+		# user top artists
+		user_top_artist_endpoint = f"{spotify_api_url}/me/top/artists?limit=5"
+		top_artist_resp = requests.get(user_top_artist_endpoint, headers=auth_header)
+		top_artist_data = json.loads(top_artist_resp.text)
+
+		# user top tracks
+		user_top_tracks_endpoint = f"{spotify_api_url}/me/top/tracks?limit=25"
+		top_tracks_resp = requests.get(user_top_tracks_endpoint, headers=auth_header)
+		top_tracks_data = json.loads(top_tracks_resp.text)
+
+		if not User.query.filter(User.spotify_id == spotify_id).first():
+			user = User(spotify_id=spotify_id)
+			db.session.add(user)
+			db.session.commit()
+
+		return render_template("profile.jinja2", profile=profile_data, artists=top_artist_data, tracks=top_tracks_data)
---
+++ frontend/spotify_auth/spotify_auth.py
@@ -0,0 +1,79 @@
+import json
+from flask import Flask, Blueprint, request, redirect, g, render_template, session, abort
+from jinja2 import TemplateNotFound
+import requests
+from urllib.parse import quote
+import os
+# from models import db, connect_db, User, Artist, Track, Genre, Evaluation
+from flask import current_app as app
+
+# blueprint configuration
+spotify_auth_bp = Blueprint(
+	'spotify_auth_bp', __name__,
+	template_folder='templates',
+	static_folder='static'
+)
+
+# these are all for spotify
+client_id = os.environ.get("CLIENT_ID")
+client_secret = os.environ.get("CLIENT_SECRET")
+spotify_auth_url = "https://accounts.spotify.com/authorize"
+spotify_token_url = "https://accounts.spotify.com/api/token"
+spotify_api_base = "https://api.spotify.com"
+API_VERSION = "v1"
+spotify_api_url = f"{spotify_api_base}/{API_VERSION}"
+
+#  server side information
+redirect_uri = os.environ.get("REDIRECT_URI")
+# specify what permissions you need based on endpoints
+scope = "user-read-private user-read-email user-top-read playlist-modify-public playlist-modify-private"
+state = ""
+SHOW_DIALOG_bool = True
+SHOW_DIALOG_str = str(SHOW_DIALOG_bool).lower()
+
+app.config["SECRET_KEY"] = "secrets secrets are no fun"
+
+# the following spotify authentication code was taken and adapted slightly from this repo
+# https://github.com/drshrey/spotify-flask-auth-example/blob/master/main.py
+
+auth_query_parameters = {
+	"response_type": "code",
+	"redirect_uri": redirect_uri,
+	"scope": scope,
+	"client_id": client_id
+}
+
+
+@spotify_auth_bp.route('/connect', methods=['GET'])
+def spotify_auth():
+	"""app requests authorization ie redirect to spotify login page"""
+	url_args = "&".join([f'{key}={val}' for key, val in auth_query_parameters.items()])
+	auth_url = f"{spotify_auth_url}/?{url_args}"
+	return redirect(auth_url)
+
+
+@spotify_auth_bp.route("/callback")
+def callback():
+	# requests refresh and access tokens
+	auth_token = request.args['code']
+	session['code'] = auth_token
+
+	auth_options = {
+		"grant_type": "authorization_code",
+		"code": str(auth_token),
+		"redirect_uri": redirect_uri,
+		'client_id': client_id,
+		'client_secret': client_secret,
+	}
+	post_req = requests.post(spotify_token_url, data=auth_options)
+
+	# token returned to app
+	resp_data = json.loads(post_req.text)
+	access_token = resp_data["access_token"]
+	session['access_token'] = access_token
+
+	refresh_token = resp_data["refresh_token"]
+	token_type = resp_data["token_type"]
+	expires_in = resp_data["expires_in"]
+
+	return redirect('/profile')
